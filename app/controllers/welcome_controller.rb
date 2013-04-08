b00058a8543086109db3c2da1a1e44848253a494
JakobRisy <J.Risy@web.de>
2013-04-08 12:25:49 +0200

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+  def index
+	@result = ''
+  end
+  def calculate
+	@result = params[:f].to_i + params[:s].to_i
+	render :action => :index
+  end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calculate::Application.config.secret_token = '25c86488e1279569e40651638ab9ca0fb6accdbb1a21cf816700fe123531b0102741e58918b7d7b3a0cd713c3f9bba57a7a6b696c53451fc87e7f6729fa9dd2a'
